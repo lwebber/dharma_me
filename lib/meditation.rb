b2d243c055ddf61daac4583b84d23a6a535d2de9
Laura Webber <wwebby1@gmail.com>
2017-09-17 14:38:23 +0000

    add #play method

--- lib/meditation.rb
+++ lib/meditation.rb
@@ -8,4 +8,11 @@ class Meditation
     @stream = stream
   end
 
+  def play
+    puts "Click the link to play"
+    puts @stream
+  end
+
 end
+
+meditation = Meditation.new("sample", "http://marc.ucla.edu/mpeg/01_Breathing_Meditation.mp3")
