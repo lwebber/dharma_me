c889e3fe8d960e1d57ec30776afef91d585fa32e
Laura Webber <wwebby1@gmail.com>
2017-09-17 02:23:15 +0000

    add dependencies

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,7 @@
-require_relative './lib/meditation.rb'
-require_relative './lib/talk.rb'
-require_relative './lib/teacher.rb'
 require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/meditation.rb'
+require_relative '../lib/talk.rb'
+require_relative '../lib/teacher.rb'
