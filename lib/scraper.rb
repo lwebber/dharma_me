f859ff2f565f0ea128f42deb3d0832a8aa1d4d10
Laura Webber <wwebby1@gmail.com>
2017-09-17 03:22:04 +0000

    add scraping methods

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,5 +3,29 @@ require './config/environment.rb'
 
 class Scraper
 
+  # def get
+  #   html = open("https://flatironschool.com/")
+  #   doc = Nokogiri::HTML(html)
+  #   doc.css(".site-header__hero__headline")
+  # end
+
+  # html = open("https://flatironschool.com")
+  # doc = Nokogiri::HTML(html)
+  # doc.css(".site-header__hero__headline")
+  # puts doc.css(".site-header__hero__headline").text
+
+
+  def self.scrape_talk
+    html = open("http://www.audiodharma.org")
+    doc = Nokogiri::HTML(html)
+    title = doc.css("div.talk-title")
+    binding.pry
+  end
+
+  def self.scrape_meditation
+
+  end
 
 end
+
+Scraper.scrape_talk
