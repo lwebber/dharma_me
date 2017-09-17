05ed4c5b86cb0485e8dd4c3513f3853c79b5693b
Laura Webber <wwebby1@gmail.com>
2017-09-17 14:50:07 +0000

    add meditation#all

--- lib/meditation.rb
+++ lib/meditation.rb
@@ -2,14 +2,20 @@ require './config/environment.rb'
 
 class Meditation
   attr_accessor :title, :stream
+  @@all = []
 
   def initialize(title, stream)
     @title = title
     @stream = stream
+    @@all << self
   end
 
   def play
     puts "Click the link to play: #{@stream}"
+  end
+
+  def self.all
+    @@all
   end
 
 end
