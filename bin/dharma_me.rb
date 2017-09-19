4818a5735767896ccc3217d52ca5dc5a5bb4a503
Laura Webber <wwebby1@gmail.com>
2017-09-19 03:16:52 +0000

    refine CLI

--- bin/dharma_me.rb
+++ bin/dharma_me.rb
@@ -1,29 +1,31 @@
 require './config/environment.rb'
 
+    puts "Welcome to DharmaMe!"
+    puts "You must be stressed out. Why don't you take a break?"
 
-puts "Welcome to DharmaMe!"
-puts "You must be stressed out. Why don't you take a break?"
+    input = nil
+    while input != "q"
 
-input = nil
-while input != "q"
+    puts "\n\nWould you like to (1) Listen to a dharma talk? (2) Meditate or (3) Just Breathe? Or type q to quit:"
 
-puts "Would you like to (1) Listen to a dharma talk? (2) Meditate or (3) Just Breathe? Or type q to quit:"
-
-input = gets.strip
-
-  if input.to_i == 1
-    puts "Here are your choices today: "
-    Talk.list
-  elsif input.to_i == 2
-    puts "Here are your choices today: "
-    Meditation.create_from_collection(Scraper.scrape_meditations("http://marc.ucla.edu/mindful-meditations"))
-    Meditation.list
-    puts "Which one would you like to hear?"
     input = gets.strip
-    Meditation.find_by_input(input).play
-  elsif input.to_i == 3   
-    Breathe.start
-  else
-    puts "Not sure what you want?"
+
+    if input.to_i == 1
+      puts "Here are your choices today: "
+      Talk.list
+    elsif input.to_i == 2
+      puts "Here are your choices today: "
+      Meditation.reset!
+      Meditation.create_from_collection(Scraper.scrape_meditations("http://marc.ucla.edu/mindful-meditations"))
+      Meditation.list
+      puts "\nWhich one would you like to hear? (or q to quit, m for main menu)"
+      input = gets.strip
+      if input != "q"
+        Meditation.find_by_input(input).play
+      end    
+    elsif input.to_i == 3
+      Breathe.start
+    end
   end
-end
+
+
