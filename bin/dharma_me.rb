396c544e7ff011626434e77ee895ce44f7022c1c
Laura Webber <wwebby1@gmail.com>
2017-09-19 01:50:04 +0000

    add breathe.rb and CLI code

--- bin/dharma_me.rb
+++ bin/dharma_me.rb
@@ -2,9 +2,26 @@ require './config/environment.rb'
 
 
 puts "Welcome to DharmaMe!"
-puts "You must be stressed out. Why don't you take a break to meditate?"
-puts "Pick a meditation"
+puts "You must be stressed out. Why don't you take a break?"
+
+while input != "Q"
+
+puts "Would you like to (1) Listen to a dharma talk? (2) Meditate or (3) Just Breathe? Or type Q to quit:"
+
+input = gets.strip
+
+  case input
+  when input.to_i == 1
+    puts "Here are your choices today: "
+  when input.to_i == 2
+    puts "Here are your choices today: "
+    Meditation.list
+  when input.to_i == 3
+    Breathe.start
+  end
+end
+
+
+
 
 #Meditation.create_from_collection(Scraper.scrape_meditations("http://marc.ucla.edu/mindful-meditations"))
-puts "Here are your choices today: "
-Meditation.list
