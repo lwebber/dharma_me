cbc3d71d9955ef14de36655cca3ebdf828146a63
Laura Webber <wwebby1@gmail.com>
2017-10-25 00:37:09 +0000

    add #breathe comment and require_all to gemfile

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,4 @@ require 'nokogiri'
 require 'open-uri'
 require 'rainbow'
 
-require_relative "../lib/dharma_me/scraper.rb"
-require_relative "../lib/dharma_me/meditation.rb"
-require_relative "../lib/dharma_me/talk.rb"
-require_relative "../lib/dharma_me/breathe.rb"
-require_relative "../lib/dharma_me/cli.rb"
+require_all '../lib/dharma_me'
--- lib/dharma_me/breathe.rb
+++ lib/dharma_me/breathe.rb
@@ -3,7 +3,7 @@ class Breathe
   def self.start
     breathe = ["B", "R", "E", "A", "T", "H", "E"]
 
-    1.times do #production version will have more loops. Set at 1 for testing purposes.
+    1.times do #production version can have more loops. Set at 1 for testing purposes. Could modify to have user select amount of time for breathing.
       breathe.each do |letter|
         print Rainbow(letter).cyan
         sleep(1)
